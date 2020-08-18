c5a8729a57b5365f3e2c3c2bae0f0e00e1cda5b6
Travis Gandy <travis_gandy@yahoo.com>
2020-08-18 21:08:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
 
   patch '/articles/:id' do #edit action
     @article = Article.find_by_id(params[:id])
-    @article.name = params[:title]
+    @article.title = params[:title]
     @article.content = params[:content]
     @article.save
     redirect to "/articles/#{@article.id}"
